18be4ceac594b2d9eb92090059d7671fc798c3c1
Víctor Fernández <victor.fernandez.gabriel@gmail.com>
2022-08-17 13:10:04 +0200

    --wip-- [skip ci]

--- docker/init.sh
+++ docker/init.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
 (cd /pilot && nohup npm run start &)
-exec /dockerstartup/kasm_default_profile.sh /dockerstartup/vnc_startup.sh /dockerstartup/kasm_startup.sh --wait
+/dockerstartup/kasm_default_profile.sh /dockerstartup/vnc_startup.sh /dockerstartup/kasm_startup.sh --wait &
+
+# Wait for any process to exit
+wait -n  
+# Exit with status of process that exited first
+exit $?
