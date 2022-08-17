03c42a1022cd0c9513b6a6d8536ff423f2869b3c
Víctor Fernández <victor.fernandez.gabriel@gmail.com>
2022-08-17 03:15:50 +0200

    feat: add basic getters to pilot

--- pilot/src/index.ts
+++ pilot/src/index.ts
@@ -1,50 +1,10 @@
-import CDP from 'chrome-remote-interface'
+import { Browser } from 'services/browser'
 
-async function getCoords(cssSelector: string) {
-  let client
-  try {
-    // connect to endpoint
-    client = await CDP({ host: '', port: 16667 })
-    // extract domains
-    const { Runtime } = client
-    // enable events then start!
-    await Promise.all([Runtime.enable()])
+const browser = new Browser({ host: '', port: 1 })
 
-    // get clientRect of links
-    const result = await Runtime.evaluate({
-      expression: `var targetCoordEl = document.querySelector('${cssSelector}'); if (targetCoordEl) { JSON.stringify(targetCoordEl.getClientRects()); }`,
-    })
-
-    // get offset screen positioning
-    const screenPos = await Runtime.evaluate({
-      expression:
-        'JSON.stringify({offsetY: window.screen.height - window.innerHeight, offsetX: window.screen.width - window.innerWidth})',
-    })
-
-    const offset = JSON.parse(screenPos.result.value)
-    let clientRect = null
-
-    try {
-      clientRect = JSON.parse(result.result.value)['0']
-    } catch (err) {
-      return null
-    }
-
-    const retVal = {
-      x: offset.offsetX + clientRect.x,
-      y: offset.offsetY + clientRect.y,
-      width: clientRect.width,
-      height: clientRect.height,
-    }
-    console.log(cssSelector, retVal)
-    return retVal
-  } catch (err) {
-    console.error(err)
-  } finally {
-    if (client) {
-      await client.close()
-    }
-  }
-}
-
-getCoords('div')
+;(async () => {
+  await browser.navigateTo('https://mfus.tk')
+  console.log(await browser.getPageSource())
+  console.log(await browser.getCoords('button'))
+  await browser.close()
+})()
---
+++ pilot/src/services/browser/index.ts
@@ -0,0 +1,71 @@
+import CDP from 'chrome-remote-interface'
+
+export class Browser {
+  private client: Promise<CDP.Client>
+  private runtime: Promise<CDP.Client['Runtime']>
+  private page: Promise<CDP.Client['Page']>
+  private dom: Promise<CDP.Client['DOM']>
+
+  constructor(cdpOptions?: CDP.Options | undefined) {
+    const initResult = CDP(cdpOptions).then(async (client) => {
+      const { Runtime, Page, DOM } = client
+      await Promise.all([Runtime.enable(), Page.enable()])
+      return { runtime: Runtime, client, page: Page, dom: DOM }
+    })
+    this.client = new Promise(async (res) => res((await initResult).client))
+    this.runtime = new Promise(async (res) => res((await initResult).runtime))
+    this.page = new Promise(async (res) => res((await initResult).page))
+    this.dom = new Promise(async (res) => res((await initResult).dom))
+  }
+
+  async close() {
+    return (await this.client).close()
+  }
+
+  async navigateTo(url: string, timeout = 30000) {
+    const page = await this.page
+    return new Promise<void>(async (resolve, reject) => {
+      setTimeout(() => reject(new Error('Timeout')), timeout)
+      page.on('frameNavigated', () => resolve())
+      await page.navigate({ url })
+    })
+  }
+
+  async getCoords(cssSelector: string): Promise<{ x: number; y: number; width: number; height: number } | null> {
+    const runtime = await this.runtime
+    const result = await runtime.evaluate({
+      expression: `var targetCoordEl = document.querySelector('${cssSelector}'); if (targetCoordEl) { JSON.stringify(targetCoordEl.getClientRects()); }`,
+    })
+
+    const screenPos = await runtime.evaluate({
+      expression:
+        'JSON.stringify({offsetY: window.screen.height - window.innerHeight, offsetX: window.screen.width - window.innerWidth})',
+    })
+
+    const offset = JSON.parse(screenPos.result.value)
+    let clientRect: null | { x: number; y: number; width: number; height: number } = null
+
+    try {
+      clientRect = JSON.parse(result.result.value)['0']
+    } catch (err) {
+      return null
+    }
+
+    const retVal = {
+      x: offset.offsetX + clientRect!.x,
+      y: offset.offsetY + clientRect!.y,
+      width: clientRect!.width,
+      height: clientRect!.height,
+    }
+    return retVal
+  }
+
+  async getPageSource() {
+    const dom = await this.dom
+    const rootNode = await dom.getDocument({ depth: -1 })
+    const pageSource = await dom.getOuterHTML({
+      nodeId: rootNode.root.nodeId,
+    })
+    return pageSource.outerHTML
+  }
+}
