0e6f4002631b3c5fca7453de9df8d7c02c50e842
Víctor Fernández <victor.fernandez.gabriel@gmail.com>
2022-08-22 16:22:48 +0200

    --wip-- [skip ci]

--- docker/init/pilot.sh
+++ docker/init/pilot.sh
@@ -4,7 +4,7 @@ echo -n "Waiting for Chrome to be ready .."
 for i in `seq 1 40`; do
   echo -n .
   sleep 1
-  [ $i == 40 ] && echo " Timeout!" >&2; exit 1
+  [ $i = 40 ] && echo " Timeout!" >&2; exit 1
   nc -z localhost 16666 && echo " Open." && break
 done;
 
